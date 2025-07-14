529518061997069f159e6057adb01820f02e6c9d
W1_83838_Ajay <ajaysurse1487@gmail.com>
2025-07-14 20:55:37 +0530

    Find missing

---
+++ Day02/findMissingNumber.java
@@ -0,0 +1,27 @@
+public class findMissingNumber {
+    public static int findMissing(int arr[]){
+        int flag=0;
+        // use Linear search
+        for(int i=1; i<=arr.length; i++){
+            flag =0;
+            for(int j=0; j<arr.length; j++){
+                if(arr[j]==i){
+                    flag=1;
+                }
+            }
+            if(flag==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[]= {1,2,4,5,6};
+        int missingNumber = findMissing(arr);
+        System.out.println("Missing Number" + missingNumber);
+
+        
+    }
+    
+}

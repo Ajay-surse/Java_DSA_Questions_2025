710a892645578eb307ba9b48d20fe8931638bcfd
W1_83838_Ajay <ajaysurse1487@gmail.com>
2025-07-16 06:08:55 +0530

    updated changes

--- Day03/FindMissingNumber.java
+++ Day03/FindMissingNumber.java
@@ -4,6 +4,30 @@ public class FindMissingNumber {
 
     // Leetcode link : https://leetcode.com/problems/missing-number/description/
 
+
+
+    // better 
+
+    public static int usingHash(int arr[], int n){
+        int hash[]= new int[n+1];  // hash array suppose n is 5 it will 6 size of hash array in that value store from 0 to 5 index 
+
+        //storing the frequencies 
+        for(int i=0; i<n-1; i++)  {
+            //hash[arr[i]] = 1 ;
+            hash[arr[i]]++; 
+        }
+        
+        // checking the frequencies from 1 to N
+        for(int i=1; i<=n; i++){
+            if(hash[i] == 0){
+                return i;   
+            }
+        }
+
+        return -1;
+    }
+    
+    // optimal 
     public static int findMissing(int nums[],int n){
         int xor1=0,xor2=0;
         for(int i=0; i<n-1; i++){
@@ -14,11 +38,14 @@ public class FindMissingNumber {
         return(xor1^xor2);
 
     }
+
+    
     public static void main(String[] args) {
         int n=5;
         int nums[]= {1,2,4,5};
-        int ans = findMissing(nums,n);
-        System.out.print("nums : " + ans);
+      //  int ans = findMissing(nums,n);
+        int usinghash = usingHash(nums,n);
+        System.out.print("nums : " + usinghash);
         
     }
     
@@ -28,47 +55,3 @@ public class FindMissingNumber {
 
 
 
-// import java.util.*;
-
-// public class tUf {
-//     public static int missingNumber(int []a, int N) {
-
-//         //1,2,4,5
-
-//         // Outer loop that runs from 1 to N:
-//         for (int i = 1; i <= N; i++) {
-
-//             // flag variable to check
-//             //if an element exists
-//             int flag = 0;
-
-//             //Search the element using linear search:
-//             for (int j = 0; j < N - 1; j++) {   // 0 to 4
-//                 if (a[j] == i) {
-
-//                     // i is present in the array:
-//                     flag = 1;
-//                     break;
-//                 }
-//             }
-
-//             // check if the element is missing
-//             //i.e flag == 0:
-
-//             if (flag == 0) return i;
-//         }
-
-//         // The following line will never execute.
-//         // It is just to avoid warnings.
-//         return -1;
-//     }
-
-//     public static void main(String args[]) {
-//         int N = 5;
-//         int a[] = {1, 2, 4, 5};
-
-//         int ans = missingNumber(a, N);
-//         System.out.println("The missing number is: " + ans);
-//     }
-
-// }

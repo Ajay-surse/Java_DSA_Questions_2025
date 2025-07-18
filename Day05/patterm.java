eed44d6bcf26b99502c61428307a0a5c239ce2d3
W1_83838_Ajay <ajaysurse1487@gmail.com>
2025-07-18 06:00:53 +0530

    Updated pattern

--- Day05/patterm.java
+++ Day05/patterm.java
@@ -32,6 +32,27 @@ class pattern{
         }
 
     }
+    public static void pattern5(int n){
+        /*
+            * 
+         * *  *
+        * * * * *
+
+        */
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+            for(int k=0; k<2*i+1; k++){
+                System.out.print("*");
+            }
+            for(int l=0; l<n-i-1; l++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        
+    }
     
     public static void main(String[] args) {
         System.out.print("Enter the Number : ");
@@ -42,6 +63,8 @@ class pattern{
         pattern2(n);
         System.out.println("");
         patter3(n);
+        System.out.println("");
+        pattern5(n);
        
     }
 }

f8b76bae04e1879ec9665827324b7368b7486eac
W1_83838_Ajay <ajaysurse1487@gmail.com>
2025-07-22 20:56:08 +0530

    updated one

---
+++ Day08/Recursion4.java
@@ -0,0 +1,21 @@
+package Day08;
+
+import java.util.Scanner;
+
+public class Recursion4 {
+
+    public static void printReverse(int i, int n){
+        if(n<=i) return;
+        System.out.print(n + " ");
+        printReverse(i, n-1);
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter the Number : ");
+        Scanner sc= new Scanner(System.in);
+        int number = sc.nextInt();   //5 
+        int i=0;
+        printReverse(i, number);  // (0,5)
+        
+    }
+    
+}

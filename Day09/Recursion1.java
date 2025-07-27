0b32ae5bee782b21f454761e24dab106103a477e
W1_83838_Ajay <ajaysurse1487@gmail.com>
2025-07-27 10:07:36 +0530

    Recursion using Functional Way

--- Day09/Recursion1.java
+++ Day09/Recursion1.java
@@ -3,10 +3,10 @@ package Day09;
 import java.util.Scanner;
 
 public class Recursion1 {
-
+     // 5 -> 5+4+3+2+1 = 15
     // sum of first N numbers 
 
-    //paremetrized way
+    //using Parameterisd way
     public static void printFirstNNumber(int i, int sum){
         if(i<1){
             System.out.print(sum + " ");
@@ -16,10 +16,22 @@ public class Recursion1 {
 
     }
 
+    // using functional way 
+    public static int printUsingFunctionalWay(int n){
+        if(n==0){
+            return 0; 
+        }
+        return n + printUsingFunctionalWay(n-1);  // incomplete function once it is completed it will return 
+
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n= sc.nextInt();
-        printFirstNNumber(n,0);
+        //printFirstNNumber(n,0);
+        int result = printUsingFunctionalWay(n);
+        System.out.println("Result : " + result);
     }
     
 }
